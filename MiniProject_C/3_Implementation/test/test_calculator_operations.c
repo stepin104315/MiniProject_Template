a0c40458c8e9e703205e376d19af1625cbd82188
stepin104315 <71546730+stepin104315@users.noreply.github.com>
2020-09-19 14:28:35 +0530

    Update test_calculator_operations.c

--- MiniProject_C/3_Implementation/test/test_calculator_operations.c
+++ MiniProject_C/3_Implementation/test/test_calculator_operations.c
@@ -20,7 +20,6 @@ int main() {
   CU_pSuite suite = CU_add_suite(PROJECT_NAME, 0, 0);
 /* Note: Do not edit END */
   
-  
   /* Add your test functions in this format for testing*/
   CU_add_test(suite, "add", test_add);
   CU_add_test(suite, "subtract", test_subtract);
